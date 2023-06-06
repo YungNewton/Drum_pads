7f2d319c35bcc202384e7f78285918b8020fc0d1
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-06-06 23:41:16 +0100

    commit

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function playsound(){
-
+    alert(clicked);
 }
-document.querySelector(".pads").onclick = function(){playsound()};
+document.querySelector("button").onclick = function(){playsound()};
+// for( var i = 0; i < document.querySelectorAll(".pads").length; i++){
+//     document.querySelectorAll(".pads")[i].onclick = function(){playsound()};
+// }
